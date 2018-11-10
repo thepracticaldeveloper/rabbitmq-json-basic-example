d3cdbb18e4cfd9ec48d5a84909c7c6efc207a8a5
Moises Macero <mechero@users.noreply.github.com>
2018-11-10 09:49:36 +0100

    Changed Message class

--- src/main/java/io/tpd/rabbitmqexample/PracticalTipListener.java
+++ src/main/java/io/tpd/rabbitmqexample/PracticalTipListener.java
@@ -2,7 +2,7 @@ package io.tpd.rabbitmqexample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
+import org.springframework.messaging.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
